f36c7dc0ced92e32b40881da58faec22a92dab6b
Cristiano Lopes Santos <crlsantos@firjan.com.br>
2023-04-17 22:23:54 -0300

    Busca binária O(log n)

---
+++ pesquisa_binaria.py
@@ -0,0 +1,23 @@
+def pesquisa_binaria(lista, item):
+    baixo = 0
+    alto = len(lista) - 1
+
+    while baixo <= alto:
+        meio = (baixo + alto) // 2
+        chute = lista[meio]
+
+        if chute == item:
+            return meio
+        if chute > item:
+            alto = meio  + 1
+        else:
+            baixo = meio + 1
+        return None
+
+minha_lista = [1,3,5,7,9]
+alvo = 3
+indice = pesquisa_binaria(minha_lista, alvo)
+if indice != -1:
+    print(f'O valor {alvo} foi encontrado no índice {indice} do array.')
+else:
+    print(f'O valor {alvo} não foi encontrado no array.')
